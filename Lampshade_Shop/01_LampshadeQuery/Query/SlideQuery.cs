764180688d307266044ed4cbf7949607ccbeaf2b
Yaser68 <yassermohamadipour@gmail.com>
2022-06-15 13:02:26 +0430

    UI Index Injected & Slider Section implemented

---
+++ Lampshade_Shop/01_LampshadeQuery/Query/SlideQuery.cs
@@ -0,0 +1,39 @@
+﻿using _01_LampshadeQuery.Contracts.Slide;
+using ShopManagement.Infrastructure.EFCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_LampshadeQuery.Query
+{
+    public class SlideQuery : ISlideQuery
+    {
+
+        private readonly ShopContext _shopContext;
+
+        public SlideQuery(ShopContext shopContext)
+        {
+            _shopContext = shopContext;
+        }
+
+        public List<SlideQueryModel> GetSlide()
+        {
+            return _shopContext.slides.Where(x=>x.IsRemoved==false)
+                .Select(x => new SlideQueryModel 
+                { 
+                    Picture=x.Picture,
+                    PictureAlt=x.PictureAlt,
+                    PictureTitle=x.PictureTitle,
+                    Heading=x.Heading,
+                    Title=x.Title,
+                    Text=x.Text,
+                    BtnText=x.BtnText,
+                    Link=x.Link
+                    
+                })
+                .ToList();
+        }
+    }
+}
