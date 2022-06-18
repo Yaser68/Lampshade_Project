a0007a53eda16f4e5f47fb3a511392865c6339af
Yaser68 <yassermohamadipour@gmail.com>
2022-06-18 09:05:10 +0430

    Product Category section of UI  implemented

---
+++ Lampshade_Shop/01_LampshadeQuery/Query/ProductCategoryQuery.cs
@@ -0,0 +1,36 @@
+﻿using _01_LampshadeQuery.Contracts.ProductCategory;
+using ShopManagement.Infrastructure.EFCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_LampshadeQuery.Query
+{
+    public class ProductCategoryQuery : IProductCategoryQuery
+    {
+        private readonly ShopContext _shopContext;
+
+        public ProductCategoryQuery(ShopContext shopContext)
+        {
+            _shopContext = shopContext;
+        }
+
+        public List<ProductCategoryQueryModel> GetProductCategories()
+        {
+            return _shopContext.productCategories.Select(x => new ProductCategoryQueryModel
+            {
+                Name= x.Name,
+                Picture= x.Picture,
+                Description= x.Description,
+                PictureAlt= x.PictureAlt,
+                PictureTitle= x.PictureTitle,
+                Slug= x.Slug,
+                Keywords=x.Keywords,
+                MetaDescription= x.MetaDescription
+
+            }).ToList();  
+        }
+    }
+}
---
+++ Lampshade_Shop/ServiceHost/ViewComponents/ProductCategoryViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using _01_LampshadeQuery.Contracts.ProductCategory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.ViewComponents
+{
+    public class ProductCategoryViewComponent : ViewComponent
+    {
+
+        private readonly IProductCategoryQuery _productCategories;
+
+        public ProductCategoryViewComponent(IProductCategoryQuery productCategories)
+        {
+            _productCategories = productCategories;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+
+            var productCategories=_productCategories.GetProductCategories();
+
+            return View(productCategories);
+        }
+
+    }
+}
